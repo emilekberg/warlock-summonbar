c79e108b75a1f66048fbd900ff65ac6c87d059ec
Emil Ekberg <ekberg.emil@gmail.com>
2024-12-15 14:24:28 +0100

    cooldown and restructure

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 local buttonSize = 32
 local buttonPadding = 2
 local healthstones = {"Create Healthstone (Minor)", "Create Healthstone (Lesser)", "Create Healthstone", "Create Healthstone (Greater)", "Create Healthstone (Major)"}
-local spells = {"Ritual Of Summoning", "Create Soulstone", healthstones, "Summon Imp", "Summon Voidwalker", "Summon Succubus", "Summon Felhunter"}
 local frame = CreateFrame("Frame", "Backdrop", UIParent, "BackdropTemplate")
 
 frame:SetMovable(true)
@@ -18,50 +17,29 @@ frame:SetScript("OnDragStop", function(self)
     self:StopMovingOrSizing()
 end)
 
-function IsArray(t)
-    if type(t) ~= "table" then
-        return false
-    end
-    local i = 1
-    for key in pairs(t) do
-        if key ~= i then
-            return false
-        end
-        i = i+1
-    end
-    return true
-end
-
-function CreateButtonForSpell(spell, i)
-    local highestRankSpell = spell
+function GetHighestLevelHealthstone()
     local spellinfo
-    if IsArray(spell) then
-        for spellIndex = 1, #spell do
-            highestRankSpell = spell[spellIndex]
-            local tmpSpellInfo = C_Spell.GetSpellInfo(spell[spellIndex])
-            if tmpSpellInfo ~= nil and IsSpellKnown(tmpSpellInfo.spellID) then
-                highestRankSpell = spell[spellIndex]
-                spellinfo = C_Spell.GetSpellInfo(highestRankSpell)
-            end
+    for i = 1, #healthstones do
+        -- highestRankSpell = spell[spellIndex]
+        local tmpSpellInfo = C_Spell.GetSpellInfo(healthstones[i])
+        if tmpSpellInfo ~= nil and IsSpellKnown(tmpSpellInfo.spellID) then
+            spellinfo = tmpSpellInfo
         end
-    else
-        highestRankSpell = spell
-        spellinfo = C_Spell.GetSpellInfo(highestRankSpell)
     end
-    
-    local button = CreateFrame("Button", "MySpellButton", frame, "SecureActionButtonTemplate")
+    return spellinfo.name
+end
+function CreateButtonForSpell(spellinfo, i)
+    local button = CreateFrame("Button", spellinfo.name .. "button", frame, "SecureActionButtonTemplate")
     button:SetSize(buttonSize, buttonSize)  -- Set button size (adjust as needed)
     button:SetPoint("LEFT", frame, "LEFT", ((i-1)*(buttonSize + buttonPadding)), 0)  -- Position the button on screen
 
-    -- Set the button's icon (optional)
     local icon = button:CreateTexture(nil, "BACKGROUND")
     icon:SetAllPoints()
---    icon:SetTexture("Interface\\Icons\\Spell_Nature_MoonGlow")  -- Replace with the appropriate spell icon
     icon:SetTexture(spellinfo.iconID)
-    -- Set up the button to cast a spell
+    
     button:SetAttribute("type", "spell")  -- Set the type of action (casting a spell)
-    button:SetAttribute("spell", spell)  -- Replace with the spell you want to cast
-
+    button:SetAttribute("spell", spellinfo.spellID)  -- Replace with the spell you want to cast
+    
     button:Show()
 
     button:SetScript("OnEnter", function (self)
@@ -74,8 +52,14 @@ function CreateButtonForSpell(spell, i)
         GameTooltip:Hide()
     end)
 
+    local cooldown = CreateFrame("Cooldown", spellinfo.name .. "cooldown", frame, "CooldownFrameTemplate")
+    cooldown:SetAllPoints()
+
     frame:SetWidth(i * (buttonSize + buttonPadding))
 end
+local spells = {"Ritual Of Summoning", "Create Soulstone", GetHighestLevelHealthstone(), "Summon Imp", "Summon Voidwalker", "Summon Succubus", "Summon Felhunter"}
 for i=1, #spells do
-    CreateButtonForSpell(spells[i], i)
+    local spellinfo = C_Spell.GetSpellInfo(spells[i])
+    CreateButtonForSpell(spellinfo, i)
+
 end
